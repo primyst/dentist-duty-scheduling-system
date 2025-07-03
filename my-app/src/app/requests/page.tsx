84de249206455d7f4b95ea283b5bfd8332b851f1
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-07-03 23:08:38 +0100

    Allahu Akbar

--- my-app/src/app/requests/page.tsx
+++ my-app/src/app/requests/page.tsx
@@ -1,1 +1,7 @@
-
+export default function RequestsPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-gray-600 text-xl">
+      UNDER DEVELOPMENT 🚧
+    </div>
+  );
+}
