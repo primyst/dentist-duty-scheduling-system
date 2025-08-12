4b2ec2c8bfff4548c5cb3d8f43338f5e13f92026
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-12 23:30:13 +0100

    Update layout.tsx

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,18 +1,18 @@
+"use client";
+
 import "./globals.css";
 import type { Metadata } from "next";
 import AppShell from "@/components/AppShell";
-import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Qudus Lautech Medical Duties Scheduling App",
-  description:
-    "Manage shifts for doctors and nurses, developed by Abdullateef Abdulqudus",
+  description: "Manage shifts for doctors and nurses, developed by Abdullateef Abdulqudus",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-gray-100">
         <AppShell>{children}</AppShell>
       </body>
     </html>
