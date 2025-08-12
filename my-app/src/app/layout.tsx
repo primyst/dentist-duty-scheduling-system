7a25a227e5202c71fd688134757a4a7f703abf5e
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-12 16:04:55 +0100

    Update layout.tsx

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
-// layout.tsx
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
+import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Qudus Lautech Medical Duties Scheduling App",
@@ -14,17 +14,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const hideSidebar = pathname === "/login";
+
   return (
     <html lang="en">
       <body className="bg-gray-100">
         <div className="flex min-h-screen">
-          
-          <aside className="hidden lg:block w-64 bg-white shadow-lg">
-            <Sidebar />
-          </aside>
+          {!hideSidebar && (
+            <aside className="hidden lg:block w-64 bg-white shadow-lg">
+              <Sidebar />
+            </aside>
+          )}
 
           <div className="flex-1">
-            <Sidebar />
+            {!hideSidebar && <Sidebar />}
             <main className="p-6">{children}</main>
           </div>
         </div>
