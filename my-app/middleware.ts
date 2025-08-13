19f5618180eb555b9c30c95e54682ad395af6466
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-13 13:41:31 +0100

    Update middleware.ts

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,18 +1,17 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
+const EXPIRE_TIME = 1 * 60 * 60 * 1000; // 1 hour
 
 export function middleware(req: NextRequest) {
-  const { cookies, nextUrl } = req;
+  const cookies = req.cookies;
   const token = cookies.get("authToken");
-
-  if (!token) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
   const tokenTimestamp = cookies.get("authTokenTimestamp");
-  if (tokenTimestamp && Date.now() - parseInt(tokenTimestamp) > EXPIRE_TIME) {
+
+  if (
+    tokenTimestamp &&
+    Date.now() - parseInt(tokenTimestamp.value) > EXPIRE_TIME
+  ) {
     // Token expired
     const res = NextResponse.redirect(new URL("/", req.url));
     res.cookies.delete("authToken");
@@ -20,9 +19,13 @@ export function middleware(req: NextRequest) {
     return res;
   }
 
+  if (!token && !req.nextUrl.pathname.startsWith("/")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/staff/:path*", "/requests/:path*", "/stats/:path*"],
+  matcher: ["/dashboard/:path*"],
 };
