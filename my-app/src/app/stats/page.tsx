7393bf7fe6c9fa003d770a8a6285ffe3a21a0270
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-13 09:45:00 +0100

    Update page.tsx

--- my-app/src/app/stats/page.tsx
+++ my-app/src/app/stats/page.tsx
@@ -1,11 +1,15 @@
 import StatsPage from "@/components/Stats";
 import DepartmentStaffBarChart from "@/components/DepartmentStaffBarChart";
+import Sidebar from "@/components/Sidebar";
 
 export default function Stats() {
   return (
+   <div>
+    <Sidebar />
     <main className="space-y-6 p-4">
       <DepartmentStaffBarChart />
       <StatsPage />
     </main>
+   </div>
   );
 }
