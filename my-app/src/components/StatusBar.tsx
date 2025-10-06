043af976fc60a4d5444487a75d42411889994628
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-06 13:00:57 +0100

    Update StatusBar.tsx

--- my-app/src/components/StatusBar.tsx
+++ my-app/src/components/StatusBar.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const StatusBar = () => (
-  <div className="flex justify-around p-2 bg-gray-200 mt-4 border-t">
+  <div className="flex justify-around p-2 bg-gray-200 mt-4 border-t rounded">
     <div className="flex items-center gap-1"><div className="w-4 h-4 bg-blue-300"></div> Morning</div>
     <div className="flex items-center gap-1"><div className="w-4 h-4 bg-green-300"></div> Afternoon</div>
     <div className="flex items-center gap-1"><div className="w-4 h-4 bg-red-400"></div> Conflict</div>
