8fb4b09b3af966e03d2e5b2ccc7fa6b7dd60a803
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-06 13:11:22 +0100

    Update AdminPanel.tsx

--- my-app/src/components/AdminPanel.tsx
+++ my-app/src/components/AdminPanel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "@/components/Header";
 import ScheduleTable from "@/components/ScheduleTable";
 import StatsPanel from "@/components/StatsPanel";
